b76272830be87c89aa117db8fa69698006ad0ae6
nairobi-j <jerinusrat001@gmail.com>
2024-11-30 15:28:10 +0600

    initiated log in page

---
+++ public/Firstpage.jsx
@@ -0,0 +1,22 @@
+
+
+function Firstpage(){
+    return(
+        <>
+        <form>
+            <p>
+                Email
+            </p>
+            <input type= "email" name= "email" placeholder="Jerin@gmail.com" />
+            <p>
+                Password
+            </p>
+            <input type= "password" name= "password" placeholder="as^5+@ghifht?" />
+        </form>
+        <button>Submit</button>
+        </>
+
+    );
+}
+
+export default Firstpage
